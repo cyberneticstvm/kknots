f3208ca0acc48c6dba7b94808f4f36bbbb9d2a47
Cybernetics <mail@cybernetics.me>
2024-06-16 12:53:43 +0530

    small updates

--- app/Http/Controllers/admin/ReportController.php
+++ app/Http/Controllers/admin/ReportController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
+use App\Models\Plan;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -11,27 +15,43 @@ class ReportController extends Controller
     {
         $inputs = array(date('Y-m-d'), date('Y-m-d'), $plan = '');
         $data = collect();
-        return view('admin.report.registration', compact('inputs', 'data'));
+        $plans = Plan::pluck('name', 'id');
+        return view('admin.report.registration', compact('inputs', 'data', 'plans'));
     }
 
-    public function registrationFetch()
+    public function registrationFetch(Request $request)
     {
-        $inputs = array(date('Y-m-d'), date('Y-m-d'), $plan = '');
-        $data = collect();
-        return view('admin.report.registration', compact('inputs', 'data'));
+        $request->validate([
+            'from_date' => 'required|date',
+            'to_date' => 'required|date'
+        ]);
+        $inputs = array($request->from_date, $request->to_date, $request->plan);
+        $plans = Plan::pluck('name', 'id');
+        $data = User::withTrashed()->whereBetween('created_at', [Carbon::parse($request->from_date)->startOfDay(), Carbon::parse($request->to_date)->endOfDay()])->where('role', 21)->when($request->plan, function ($q) use ($request) {
+            return $q->where('plan', $request->plan);
+        })->latest()->get();
+        return view('admin.report.registration', compact('inputs', 'data', 'plans'));
     }
 
     public function payment()
     {
         $inputs = array(date('Y-m-d'), date('Y-m-d'), $plan = '');
+        $plans = Plan::pluck('name', 'id');
         $data = collect();
-        return view('admin.report.payment', compact('inputs', 'data'));
+        return view('admin.report.payment', compact('inputs', 'data', 'plans'));
     }
 
-    public function paymentFetch()
+    public function paymentFetch(Request $request)
     {
-        $inputs = array(date('Y-m-d'), date('Y-m-d'), $plan = '');
-        $data = collect();
-        return view('admin.report.payment', compact('inputs', 'data'));
+        $request->validate([
+            'from_date' => 'required|date',
+            'to_date' => 'required|date'
+        ]);
+        $inputs = array($request->from_date, $request->to_date, $request->plan);
+        $plans = Plan::pluck('name', 'id');
+        $data = Payment::withTrashed()->whereBetween('created_at', [Carbon::parse($request->from_date)->startOfDay(), Carbon::parse($request->to_date)->endOfDay()])->when($request->plan, function ($q) use ($request) {
+            return $q->where('plan_id', $request->plan);
+        })->latest()->get();
+        return view('admin.report.payment', compact('inputs', 'data', 'plans'));
     }
 }
---
+++ resources/views/admin/report/payment.blade.php
@@ -0,0 +1,83 @@
+@extends("base")
+@section("content")
+<section>
+    <div class="login pro-edit-update">
+        <div class="container">
+            <div class="row">
+                <div class="inn">
+                    <div class="rhs">
+                        <div class="form-login">
+                            {{ html()->form('POST', route('admin.report.payment.fetch'))->class('')->open() }}
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label class="lb req">From Date:</label>
+                                        {{ html()->date('from_date', old('from_date') ?? $inputs[0])->class('form-control') }}
+                                        @error('from_date')
+                                        <small class="text-danger">{{ $errors->first('from_date') }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label class="lb req">To Date:</label>
+                                        {{ html()->date('to_date', old('to_date') ?? $inputs[1])->class('form-control') }}
+                                        @error('to_date')
+                                        <small class="text-danger">{{ $errors->first('to_date') }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label class="lb">Plan:</label>
+                                        {{ html()->select('plan', $plans, old('plan') ?? $inputs[2])->class('form-control')->placeholder('All') }}
+                                    </div>
+                                </div>
+                            </div>
+                            {{ html()->submit('Fetch')->class('btn btn-primary btn-submit') }}
+                            {{ html()->form()->close() }}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row mt-5">
+                <div class="col-md-8 db-sec-com db-new-pro-main">
+                    <h2 class="db-tit">Report Payments</h2>
+                </div>
+                <div class="col-md-12 col-lg-12">
+                    <div class="table-responsive">
+                        <table id="myTable">
+                            <thead>
+                                <tr>
+                                    <th>SL No</th>
+                                    <th>Full Name</th>
+                                    <th>Contact Number</th>
+                                    <th>Amount</th>
+                                    <th>Payment Date</th>
+                                    <th>Plan</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse($data as $key => $item)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $item->user->name }}</td>
+                                    <td>{{ $item->user->mobile }}</td>
+                                    <td>{{ $item->amount }}</td>
+                                    <td>{{ $item->created_at->format('d.M.Y') }}</td>
+                                    <td>{{ $item->plan->name }}</td>
+                                    <td>{!! $item->status() !!}</td>
+                                </tr>
+                                @empty
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
