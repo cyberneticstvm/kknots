64a0df041e6c18df0c4b9c77e299e2e4e6350f07
Vijoy Sasidharan <mail@cybernetics.me>
2024-08-29 16:00:57 +0400

    small updates

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,6 +53,11 @@ class User extends Authenticatable
         return ($this->deleted_at) ? "<span class='badge bg-danger'>Deleted</span>" : "<span class='badge bg-success'>Active</span>";
     }
 
+    public function redRow()
+    {
+        return ($this->deleted_at) ? "text-danger" : "";
+    }
+
     public function verified()
     {
         return ($this->verified == 1) ? "<i class='fa fa-check text-success'></i>" : "<i class='fa fa-times text-danger'></i>";
